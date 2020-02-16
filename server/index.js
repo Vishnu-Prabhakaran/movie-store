fb4939fd113d84ab40004705087a4237d533acd4
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-17 00:02:30 +1100

    Adding Db

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const apiPort = 4000;
+const db = require('./db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'Mongodb Connection Error:'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
