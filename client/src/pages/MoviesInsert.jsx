8e5a7a9919d5c6d9157676aebfbe68a51e87beba
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-02-20 00:12:22 +1100

    Insert page

--- client/src/pages/MoviesInsert.jsx
+++ client/src/pages/MoviesInsert.jsx
@@ -1,11 +1,85 @@
 import React, { Component } from 'react';
+import api from '../api';
+
+import {
+  Title,
+  Wrapper,
+  Label,
+  InputText,
+  AddButton,
+  CancelButton
+} from './Styles';
 
 class MoviesInsert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      rating: '',
+      time: ''
+    };
+  }
+
+  handleChangeInputName = async event => {
+    const name = event.target.value;
+    this.setState({ name });
+  };
+  handleChangeInpuRating = async event => {
+    const rating = event.target.validity.valid
+      ? event.target.value
+      : this.state.rating;
+
+    this.setState({ rating });
+  };
+  handleChangeInputTime = async event => {
+    const time = event.target.value;
+    this.setState({ time });
+  };
+
+  handleIncludeMovie = async () => {
+    const { name, rating, time } = this.state;
+    const arrayTime = time.split('/');
+    const payload = { name, rating, time: arrayTime };
+    await api.insertMovie(payload).then(res => {
+      window.alert('Movie Sucessfully Inserted!!');
+      this.state = {
+        name: '',
+        rating: '',
+        time: ''
+      };
+    });
+  };
   render() {
+    const { name, rating, time } = this.state;
     return (
-      <div>
-        <p> In this page you will see the form to add movies</p>
-      </div>
+      <Wrapper>
+        <Title> Create Movie</Title>
+        <Label> Name:</Label>
+        <InputText
+          type='text'
+          value={name}
+          onChange={this.handleChangeInputName}
+        />
+        <Label> Rating:</Label>
+        <InputText
+          type='number'
+          step='0.1'
+          lang='en-US'
+          min='0'
+          max='10'
+          pattern="[0-9]+([,\.][0-9]+)?"
+          value={rating}
+          onChange={this.handleChangeInpuRating}
+        />
+        <Label>Time:</Label>
+        <InputText
+          type='text'
+          value={time}
+          onChange={this.handleChangeInputTime}
+        />
+        <AddButton onClick={this.handleIncludeMovie}> Add Movie</AddButton>
+        <CancelButton href={'/movies/list'}>Cancel</CancelButton>
+      </Wrapper>
     );
   }
 }
--- client/src/pages/Styles.jsx
+++ client/src/pages/Styles.jsx
@@ -38,3 +38,33 @@ export const Delete = styled.div`
   color: #ff0000;
   cursor: pointer;
 `;
+
+export const Title = styled.h1.attrs({
+  className: 'h1'
+})``;
+
+export const Wrapper = styled.div.attrs({
+  className: 'form-group'
+})`
+  margin: 0 30px;
+`;
+
+export const Label = styled.label`
+  margin: 5px;
+`;
+export const InputText = styled.input.attrs({
+  className: 'form-control'
+})`
+  margin: 5px;
+`;
+export const AddButton = styled.button.attrs({
+  className: 'btn btn-primary'
+})`
+  margin: 15px 15px 15px 5px;
+`;
+
+export const CancelButton = styled.a.attrs({
+  className: 'btn btn-danger'
+})`
+  margin: 15px 15px 15px 5px;
+`;
